c29fc5478342bc92a4f0af5f0bdd27b9d90232ee
rpr9805 <147516960+rpr9805@users.noreply.github.com>
2023-10-23 20:50:24 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,9 +1,9 @@
-from operations import add
+from operations import multiply
 def game():
   score = 0
   while True:
     print('======== Menu ========'
-    '\n1. Add'
+    '\n1. Multiply'
     '\n0. Exit')
     option = int(input('\nChoice an option: '))
     if option == 0:
@@ -13,9 +13,9 @@ def game():
     num_2 = input('Enter second number: ')
     answer = int(input('Enter you answer: '))
     if option == 1:
-      result = add(num_1, num_2)
+      result = multiply(num_1, num_2)
       if result == answer:
-        score += 1
+        score += 2
         print('Correct!!')
       else:
         print('Incorrect')
