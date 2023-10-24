b3ae45516d6b59cce50cf20f61bebf0bb2aabfed
rpr9805 <147516960+rpr9805@users.noreply.github.com>
2023-10-23 20:57:27 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,9 +1,9 @@
-from operations import add
+from operations import power
 def game():
   score = 0
   while True:
     print('======== Menu ========'
-    '\n1. Add'
+    '\n1. Power'
     '\n0. Exit')
     option = int(input('\nChoice an option: '))
     if option == 0:
@@ -13,9 +13,9 @@ def game():
     num_2 = input('Enter second number: ')
     answer = int(input('Enter you answer: '))
     if option == 1:
-      result = add(num_1, num_2)
+      result = power(num_1, num_2)
       if result == answer:
-        score += 1
+        score += 4
         print('Correct!!')
       else:
         print('Incorrect')
